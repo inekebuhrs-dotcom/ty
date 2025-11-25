eff9bae062b8deaeefa4a6038bd4e2840368cff3
marianamafla <mariana.mafla@panam.ed.cr>
2025-11-25 13:17:24 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sprite, location) {
+    game.gameOver(true)
+})
+let Hero: Sprite = null
 let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -16,4 +20,7 @@ let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
+controller.moveSprite(Hero)
 tiles.setCurrentTilemap(tilemap`level1`)
+scene.cameraFollowSprite(Hero)
+info.startCountdown(60)
